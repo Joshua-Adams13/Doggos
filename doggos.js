0d6a97eaedce528f1d563425895dbc32229a991a
Josh Adams <joshuaadams@Joshs-MacBook-Pro.local>
2022-06-01 19:03:13 -0600

    add labs and huskies. fix css issues

--- doggos.js
+++ doggos.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const DOG_URL = "https://dog.ceo/api/breeds/image/random"
+const LAB_URL = "https://dog.ceo/api/breed/labrador/images/random"
+const HSKY_URL = "https://dog.ceo/api/breed/husky/images/random"
 
 const doggos = document.querySelector(".doggos");
 
@@ -17,8 +19,46 @@ function addNewDoggo() {
         pic.src = processedResponse.message;
         pic.setAttribute('class', 'img');
         pic.alt = "Cute doggo";
-        doggos.appendChild(pic);
+        doggos.prepend(pic);
     });
 };
 
-document.querySelector(".return-doggo").addEventListener('click', addNewDoggo);
+document.querySelector("#return-doggo").addEventListener('click', addNewDoggo);
+
+function addNewLabbo() {
+    const promise = fetch(LAB_URL);
+    promise
+    .then(function(response) {
+        const processingPromise = response.json();
+        return processingPromise;
+        
+    })
+    .then(function(processedResponse) {
+        const pic = document.createElement("img");
+        pic.src = processedResponse.message;
+        pic.setAttribute('class', 'img');
+        pic.alt = "Cute doggo";
+        doggos.prepend(pic);
+    });
+};
+
+document.querySelector("#return-labbo").addEventListener('click', addNewLabbo);
+
+function addNewHusky() {
+    const promise = fetch(HSKY_URL);
+    promise
+    .then(function(response) {
+        const processingPromise = response.json();
+        return processingPromise;
+        
+    })
+    .then(function(processedResponse) {
+        const pic = document.createElement("img");
+        pic.src = processedResponse.message;
+        pic.setAttribute('class', 'img');
+        pic.alt = "Cute doggo";
+        doggos.prepend(pic);
+    });
+};
+
+document.querySelector("#return-husky").addEventListener('click', addNewHusky);
